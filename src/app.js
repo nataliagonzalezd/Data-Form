0fcffed418df617fb7bf199d9fe6348870c37887
nataliagonzalezd <nataliagd11@hotmail.com>
2023-01-13 17:06:12 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -6,68 +6,40 @@ import "./style.css";
 window.onload = function() {
   //write your code here
 
-  console.log("Hello");
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function() {
+    "use strict";
 
-  //agregamos un evento al elemento form, primero obtenemos el elemento
-  let form = document.querySelector("form");
-  //Luego manipulamos el elemento agregandole el evento submit
-  // form.addEventListener("evento",funcion que se va activar)
-  //on
-  form.addEventListener("submit", enviarDatos);
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll(".needs-validation");
 
-  //declaramos la funcion que envia los datos del formulario
-  function enviarDatos(e) {
-    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
-    let amount = document.getElementById("inputAmount");
-    let card = document.getElementById("inputCard");
-    let cvc = document.getElementById("inputCVC");
-    let firstname = document.getElementById("inputFName");
-    let lastname = document.getElementById("inputLName");
-    let city = document.getElementById("inputCity");
-    let state = document.getElementById("inputState");
-    let postalcode = document.getElementById("inputZip");
-    let message = document.getElementsByClassName("form-control");
-    let pagos = document.getElementsByClassName("form-check-input");
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function(form) {
+      form.addEventListener(
+        "submit",
+        function(event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
 
-    if (amount.value === "") {
-      console.log("El campo amount está vacio");
-    }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
 
-    if (card.value === "") {
-      console.log("El campo card está vacio");
-    }
-
-    if (cvc.value === "") {
-      console.log("El campo cvc está vacio");
-    }
-    if (firstname.value === "") {
-      console.log("El campo firstname está vacio");
-    }
-
-    if (lastname.value === "") {
-      console.log("El campo lastname está vacio");
-    }
-
-    if (city.value === "") {
-      console.log("El campo city está vacio");
-    }
-    if (state.value === "Pick a state") {
-      console.log("El campo state está vacio");
-    }
-
-    if (postalcode.value === "") {
-      console.log("El campo postalcode está vacio");
-    }
-    if (message.value === "") {
-      console.log("El campo message está vacio");
-    }
-
+  function radioCheck() {
     let elementoActivo = document.querySelector(
       'input[name="inlineRadioOptions"]:checked'
     );
     if (elementoActivo) {
+      let r = document.getElementById("rad");
+      r.innerHTML = "";
     } else {
-      console.log("El campo target esta vacio");
+      let r = document.getElementById("rad");
+      r.innerHTML = "Please select a valid payment.";
     }
   }
 };
